016d1286ddd69228704882534f267f2f6d630539
NUSmhk <e0550349@u.nus.edu>
2021-09-13 15:06:39 +0800

    Add automatically generated tests

--- src/main/java/seedu/address/logic/parser/RemarkCommandParser.java
+++ src/main/java/seedu/address/logic/parser/RemarkCommandParser.java
@@ -8,6 +8,7 @@ import seedu.address.logic.parser.exceptions.ParseException;
 import static java.util.Objects.requireNonNull;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_REMARK;
 import seedu.address.logic.commands.RemarkCommand;
+import seedu.address.model.person.Remark;
 
 public class RemarkCommandParser implements Parser<RemarkCommand> {
 
@@ -25,7 +26,8 @@ public class RemarkCommandParser implements Parser<RemarkCommand> {
                 RemarkCommand.MESSAGE_USAGE), ive);
         }
 
-        String remark = argMultimap.getValue(PREFIX_REMARK).orElse("");
+        String remarkString = argMultimap.getValue(PREFIX_REMARK).orElse("");
+        Remark remark = new Remark(remarkString);
 
         return new RemarkCommand(index, remark);
     }
